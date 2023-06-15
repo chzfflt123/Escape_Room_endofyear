49c83422e0e7aa53097a103a9487d675ef9932d6
bomiejun <bomjun26@bergen.org>
2023-06-15 13:16:05 -0400

    srnsr

--- Inventory.js
+++ Inventory.js
@@ -5,6 +5,14 @@ function appendInventory_airconditioner_key() {
     window.location = 'airconditioner_inside.html';
 }
 
+function appendInventory_Charger() {
+    let data = [];
+    data.push('charger');
+    localStorage.setItem('inventory', JSON.stringify(data));
+    window.location = 'side4_nocharger.html';
+    console.log("inside append inventory")
+}
+
 function printImages(){
     const inventory = JSON.parse(localStorage.getItem('inventory'));
     console.log(inventory)
@@ -21,6 +29,13 @@ function printImages(){
         const img = new Image(95,85);
         img.src = "Images/airconditioner_keyicon.png";
         img.className = "keyinventory";
+        inventoryList.appendChild(img);
+        console.log("BYE")
+    }
+    if(inventory.includes('charger')){
+        const img = new Image(200,100);
+        img.src = "Images/charger_icon.png";
+        img.className = "chargerinventory";
         inventoryList.appendChild(img);
         console.log("BYE")
     }
